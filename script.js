e4a3c6916438239227c9dcff41425aad97741918
shraddhakukde <65818006+shraddhakukde@users.noreply.github.com>
2020-09-12 17:33:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+button.addEventListener("click",function(){console.log("click is working!!!!");});
+/*var database = [
+	       {   
+		      username:"shraddha",
+		      password:"secret",
+           },
+	       {   
+		      username:"sally",
+		      password:"supersecret",
+           },
+           {   
+		      username:"shina",
+		      password:"shina",
+           }
+];
+
+var newsFeed = [ 
+
+           {
+           	username:"boby",
+           	timeline:"so tired of all that learning",
+           },
+
+            {   
+		      username:"sally",
+		      timeline:"js is so cool",
+           }
+];
+
+function isValidUser(username,password){
+    for(var i=0;i<=database.length;i++){
+      if(database[i].username===username && database[i].password===password){
+      	 return true;
+      }
+
+  }
+      return false;
+}
+
+function sign_in(username,password){
+	
+      if(console.log(isUserValid(username,password))){
+      	console.log(newsFeed);
+      }else{
+      	console.log("sorry,username or password is wrong");
+      }
+      }
+
+var userNamePrompt=prompt("what is your username");
+var passwordPrompt=prompt("what is your password");
+
+sign_in(userNamePrompt,passwordPrompt);
+var todos =[  
+			"cleaning room" ,
+			"brush teeth",
+			"excercise",
+			"learning javascipt",
+			"eat healthy"
+
+];
+for(var i=0;i<5;i++ ){
+
+	todos.pop();
+}
+var counterOne = 0;
+while(counterOne<10){
+	console.log(counterOne);
+	counterOne++;
+}
+counterTwo = 10;
+do{
+	console.log(counterTwo);
+	counterTwo--;
+}while(counterTwo>0);*/
